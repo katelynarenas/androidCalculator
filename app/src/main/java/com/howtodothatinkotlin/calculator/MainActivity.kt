01c40a0cf27323097fd139119b3db33e2103a6ea
Katelyn Rodrigue <katelynrodrigue@Katelyns-MBP.fios-router.home>
2018-02-10 14:13:09 -0500

    Initial Commit

---
+++ app/src/main/java/com/howtodothatinkotlin/calculator/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.howtodothatinkotlin.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    val result by lazy { findViewById<EditText>(R.id.result) }
+    val newNumber by lazy { findViewById<EditText>(R.id.newNumber) }
+    val displayOperation by lazy {findViewById<TextView>(R.id.operation)}
+
+    val operand1 : Double? = null
+    val operand2 : Double = 0.0
+    var pendingOperation = "="
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //data input button
+        val button0 = findViewById<Button>(R.id.button0)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val buttonDot = findViewById<Button>(R.id.buttonDot)
+
+        //operation buttons
+        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
+        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+
+        val listener = View.OnClickListener { v ->
+            val b = v as Button
+            newNumber.append(b.text)
+
+        }
+
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
+        buttonDot.setOnClickListener(listener)
+
+        val opListener = View.OnClickListener { v ->
+            val op = (v as Button).text.toString()
+            val value = newNumber.text.toString()
+            if (value.isNotEmpty()){
+                performOperation(value, op)
+            }
+            pendingOperation = op
+            displayOperation.text = pendingOperation
+        }
+
+        buttonEquals.setOnClickListener(opListener)
+        buttonDivide.setOnClickListener(opListener)
+        buttonMultiply.setOnClickListener(opListener)
+        buttonMinus.setOnClickListener(opListener)
+        buttonPlus.setOnClickListener(opListener)
+    }
+
+    fun performOperation(value: String, operation: String){
+        displayOperation.text = operation
+    }
+}
