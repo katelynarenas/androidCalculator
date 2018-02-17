6e9076714e8e755c2ad7cdf2033032265698bfa1
Katelyn Rodrigue <krodrigu@tulane.edu>
2018-02-17 11:42:20 -0500

    add neg to landscape, add clear button

--- app/src/main/java/com/howtodothatinkotlin/calculator/MainActivity.kt
+++ app/src/main/java/com/howtodothatinkotlin/calculator/MainActivity.kt
@@ -16,37 +16,12 @@ const val STATE_OPERAND1_STORED = "operand1_stored"
 class MainActivity : AppCompatActivity() {
 
 
-
-    //val result by lazy {findViewById<EditText>(R.id.result)}
-    //val newNumber by lazy { findViewById<EditText>(R.id.newNumber) }
-    //val displayOperation by lazy { findViewById<TextView>(R.id.operation) }
-
     var operand1: Double? = null
     var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        //data input button
-//        val button0 = findViewById<Button>(R.id.button0)
-//        val button1 = findViewById<Button>(R.id.button1)
-//        val button2 = findViewById<Button>(R.id.button2)
-//        val button3 = findViewById<Button>(R.id.button3)
-//        val button4 = findViewById<Button>(R.id.button4)
-//        val button5 = findViewById<Button>(R.id.button5)
-//        val button6 = findViewById<Button>(R.id.button6)
-//        val button7 = findViewById<Button>(R.id.button7)
-//        val button8 = findViewById<Button>(R.id.button8)
-//        val button9 = findViewById<Button>(R.id.button9)
-//        val buttonDot = findViewById<Button>(R.id.buttonDot)
-//
-//        //operation buttons
-//        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-//        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
-//        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
-//        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-//        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
 
         val listener = View.OnClickListener { v ->
             val b = v as Button
@@ -87,10 +62,16 @@ class MainActivity : AppCompatActivity() {
         buttonPlus.setOnClickListener(opListener)
 
         buttonNegative.setOnClickListener(negListener)
+
+        buttonClear.setOnClickListener(
+                View.OnClickListener{newNumber.setText("");result.setText("")}
+        )
     }
 
     val negListener = View.OnClickListener { v ->
-        if (newNumber.text.toString() == ""){
+        if(newNumber.text.toString() == "."){
+            newNumber.setText("-0.0")
+        }else if (newNumber.text.toString() == ""){
             newNumber.setText("-")
         } else if (newNumber.text.toString() == "-"){
             newNumber.setText("")
